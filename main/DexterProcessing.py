ec54321504c42ed35c36f2ec717ae4cd0eb7c5f7
sjcusworth <samueljcuswork@protonmail.com>
2026-01-19 15:23:00 +0000

    Added gold/aurum link. Update to DexterProcessing

--- main/DexterProcessing.py
+++ main/DexterProcessing.py
@@ -1,11 +1,14 @@
+from os import rename, remove
 from csv import DictWriter, reader
 from polars import scan_csv, col, concat, Series, concat_list
 from polars import scan_parquet
-from polars import min as plmin
+from polars import min_horizontal as plmin_horizontal
 from re import sub, split
 from polars import Utf8 as plUtf8
+from polars import Int64 as plInt64
 from polars import Categorical as plCategorical
 from polars import Date as plDate
+from polars import when as plwhen
 import gc
 import pyarrow.dataset as ds
 from pyarrow.csv import CSVWriter
@@ -17,10 +20,11 @@ def rmDup(
         B_ind:int=0,
         map_file:str="./VisionToEmisMigrators.txt",
         map_delim:str="\t",
-        matching_cols:bool=True,
         low_memory:bool=True,
         wdir = "./",
+        parallel=False,
         logger = None,
+        outFile="dat_linked.parquet",
         ):
     """
     Link datasets A and B by PracticeID
@@ -44,14 +48,36 @@ def rmDup(
     Returns:
         None
     """
-    #Get names for naming of output csv
-    A_name = sub("^.*/(.*)\.csv$", r"\1", A_raw)
-    B_name = sub("^.*/(.*)\.csv$", r"\1", B_raw)
-    #Read in data
-        #infer_schema_length=0 reads all cols as utf8 (str)
-        #preventing type errors when concat
-    A_raw = scan_csv(A_raw, infer_schema_length=0, low_memory=low_memory)
-    B_raw = scan_csv(B_raw, infer_schema_length=0, low_memory=low_memory)
+    if A_raw.endswith(".csv"):
+        #Get names for naming of output csv
+        A_name = sub("^.*/(.*)\.csv$", r"\1", A_raw)
+        B_name = sub("^.*/(.*)\.csv$", r"\1", B_raw)
+        #Read in data
+            #infer_schema_length=0 reads all cols as utf8 (str)
+            #preventing type errors when concat
+        A_raw = scan_csv(A_raw, infer_schema_length=0, low_memory=low_memory)
+        B_raw = scan_csv(B_raw, infer_schema_length=0, low_memory=low_memory)
+
+    elif A_raw.endswith(".parquet"):
+        #Get names for naming of output csv
+        A_name = sub("^.*/(.*)\.parquet$", r"\1", A_raw)
+        B_name = sub("^.*/(.*)\.parquet$", r"\1", B_raw)
+        #Read in data
+            #infer_schema_length=0 reads all cols as utf8 (str)
+            #preventing type errors when concat
+        A_raw = (
+                scan_parquet(A_raw, low_memory=low_memory)
+                .with_columns(
+                    col("*").cast(plUtf8)
+                    )
+                 )
+
+        B_raw = (
+                scan_parquet(B_raw, low_memory=low_memory)
+                .with_columns(
+                    col("*").cast(plUtf8)
+                    )
+                )
 
     def practiceMatches(
             dict_pracMap:dict,
@@ -159,134 +185,74 @@ def rmDup(
         #appear in B (remove the rest from A)
     removePractice_Ids = [x for x,a,b in dict_mappings if a == True and b == True]
 
-    #Find rows to include in A
-        #Include if PATIENT_ID not in removePractice_Ids
-    f_map_rmDup = lambda x,y:True if x not in y else False
-    A_practiceID = (
-        A_raw.select(col("PRACTICE_ID"))
-        .collect().get_column("PRACTICE_ID")
-        .to_list()
-    ) #Get list of PRACTICE_ID in A
-
-    #Gets a bool mapping of rows to include in A
-    AExclude_map = [f_map_rmDup(x, y) for y in [removePractice_Ids] for x in A_practiceID]
-
-    #Bool subset of A using AExclude map
-        #Workaround using polars LazyFrame
-    A_deDup = (
-        A_raw
-        .with_columns(
-            Series("map", AExclude_map)
-        ) #Add bool map as a column
-        .filter(col("map") == True) #Filter for True in map
-        .select(
-            col("*").exclude("map") #Remove map column
-        )
-    )
-    del A_raw
-    del AExclude_map
-    gc.collect()
+    dat_dedup = concat(
+                    [
+                        B_raw,
+                        A_raw.filter(~col("PRACTICE_ID").is_in(removePractice_Ids))
+                        ],
+                    how="diagonal",
+                    parallel=parallel,)
 
     if logger is None:
         print("Saving combined data")
     else:
         logger.info("Saving combined data")
 
-        combo_raw.collect().write_csv(f"{wdir}LINKED_{A_name}_{B_name}.csv")
+    if low_memory:
+        dat_dedup.sink_parquet(f"{wdir}{outFile}")
+    else:
+        dat_dedup.collect().write_parquet(f"{wdir}{outFile}")
 
 
-def process_ethImd(
+def process_imd(
     file_dat,
     path_dir = "./",
     file_map = "imd_mapping.csv",
-    merge_imd=None, #deprecated
     imd_delim=",",
-    i_imd_key:list[int]=[0],
+    i_imd_key:int=0,
     i_imd_value=1,
-    row_group_size=None,
     low_memory=False,
     is_parquet=True,
     logger=None,
+    outFile="dat_processed.parquet",
     ):
     """
 
     """
-    if is_parquet:
-        dataset = ds.dataset(f"{path_dir}{file_dat}", format="parquet")
-        cols = list(dataset.head(1).to_pylist()[0].keys())
-        del dataset
-    else:
-        with open(f"{path_dir}{file_dat}") as f:
-            cols = f.readline().strip('\n')
-            cols = cols.split(",")
-    cols = [x for x in cols if x.find("BD") != -1 and x.find("ETH") != -1]
-    colLabs = [split(":", x)[1] for x in cols.copy()]
-    colLabs = [sub("_ETH.*$", "", x) for x in colLabs]
-
-    meta = ["PATIENT_ID", "PRACTICE_ID"]
-
-    if calcEth:
-        #Missing column required to be before all other ethnicity columns
-        i_missing = colLabs.index("MISSING")
-        x = colLabs.pop(i_missing)
-        colLabs.insert(0, x)
-        x = cols.pop(i_missing)
-        cols.insert(0, x)
-
-        meta.append("ETHNICITY")
-
+    meta = ["PATIENT_ID", "PRACTICE_ID", "PRACTICE_PATIENT_ID",]
+    joinCol = "PRACTICE_PATIENT_ID"
     imd_dict = dict()
 
-    for i_key in i_imd_key:
-        with open(f"{path_dir}{file_map}", "r") as f:
-            r = reader(f, delimiter=imd_delim)
-            label = next(r, None)[i_key]
-            label = f"IMD_{label}"
-            imd_dict[label] = dict()
-            for line in r:
-                value = str(line[i_imd_value])
-                #Ensure all values contain no special characters
-                value = sub(r'[^\w]', '', value)
+    with open(f"{path_dir}{file_map}", "r") as f:
+        r = reader(f, delimiter=imd_delim)
+        label = next(r, None)[i_imd_key]
+        label = f"IMD_{label}"
+        imd_dict[label] = dict()
+        for line in r:
+            value = str(line[i_imd_value])
+            #Ensure all values contain no special characters
+            value = sub(r'[^\w]', '', value)
 
-                if label == "IMD_pracid":
-                    imd_dict[label][f"p{line[i_key]}"] = value
-                else:
-                    imd_dict[label][f"{line[i_key]}"] = str(line[i_imd_value])
+            if label == "IMD_pracid":
+                imd_dict[label][f"p{line[i_imd_key]}"] = value
+            else:
+                imd_dict[label][f"{line[i_imd_key]}"] = str(line[i_imd_value])
 
     if is_parquet:
         q1 = (
-            scan_parquet(f"{path_dir}{file_dat}", low_memory=True)
+            scan_parquet(f"{path_dir}{file_dat}", low_memory=low_memory,)
         )
     else:
         q1 = (
-            scan_csv(f"{path_dir}{file_dat}", low_memory=True)
+            scan_csv(f"{path_dir}{file_dat}", low_memory=low_memory,)
         )
 
     q1 = (
         q1
-        .select(cols+meta)
+        .select(meta)
         .with_columns(
-            col(["PATIENT_ID"]).cast(plUtf8),
-            col(["PRACTICE_ID"]).cast(plUtf8)
+            col(meta).cast(plUtf8),
         )
-    )
-    if calcEth:
-        q1 = (
-            q1
-            .with_columns(
-                col(["ETHNICITY"]).cast(plCategorical)
-            )
-            .with_columns(
-                col("^BD.*$").str.strptime(plDate, format="%Y-%m-%d", strict=False)
-            )
-            .with_columns(
-                concat_list(col("^BD.*$")).alias("dates")
-            )
-            .with_columns(
-                    col("dates").apply(lambda x: \
-                        colLabs[x.arg_max()]
-                    ).alias("ETHNICITY_REVISED")
-            )
     )
 
     # calculating imd is now the purpose of this function (no conditional)
@@ -307,20 +273,13 @@ def process_ethImd(
             q1
             .with_columns(
                 col(lab_col).map_dict(map_imd).alias(imd_type)
+                )
             )
-        )
-    if not calcEth:
-        q1 = (
-            q1
-            .select(["PATIENT_ID"]+list(imd_dict.keys()))
-            .collect().write_parquet(f"{path_dir}dat_imd.parquet")
-        )
-    else:
-        q1 = (
-            q1
-            .select(["PATIENT_ID", "ETHNICITY_REVISED"]+list(imd_dict.keys()))
-            .collect().write_parquet(f"{path_dir}dat_imd.parquet")
-        )
+    q1 = (
+        q1
+        .select([joinCol]+list(imd_dict.keys()))
+        .collect().write_parquet(f"{path_dir}dat_imd.parquet")
+    )
 
     q1
     del q1
@@ -328,92 +287,149 @@ def process_ethImd(
 
     file_1 = f"{path_dir}{file_dat}"
     file_2 = f"{path_dir}dat_imd.parquet"
-    joinCol = "PATIENT_ID"
 
     toAdd = (
-        scan_parquet(file_2, low_memory=low_memory)
+        scan_parquet(file_2, low_memory=low_memory,)
         .with_columns(col("*").cast(plUtf8))
     )
 
     if is_parquet:
         Combine = (
             scan_parquet(file_1,
-                     low_memory=low_memory)
+                     low_memory=low_memory,)
             .with_columns(col("*").cast(plUtf8))
         )
     else:
         Combine = (
             scan_csv(file_1,
                      infer_schema_length=0,
-                     low_memory=low_memory)
+                     low_memory=low_memory,)
         )
     Combine = (
         Combine
         .join(toAdd, on=joinCol, how="left") #how="left" supports .sink_parquet and should be same as outer
     )
-    if toParquet:
-        Combine.sink_parquet(f"{path_dir}dat_processed.parquet",
-                             row_group_size=row_group_size)
+    if low_memory:
+        Combine.sink_parquet(f"{path_dir}{outFile}")
     else:
-        Combine.collect().write_csv(f"{path_dir}dat_processed.csv")
+        Combine.collect().write_parquet(f"{path_dir}{outFile}")
 
 
-def mergeCols(path_dat: str,
-    file_dat: str,
-    dict_merge: dict,
-    low_memory = False,
-    file_type = "parquet"):
+
+def mergeCols(
+        path_dat: str,
+        file_dat: str,
+        dict_merge: dict,
+        low_memory = False,
+        file_type = "parquet",
+        logger=None,
+        outFile="condMerged.parquet",
+        ):
+    """
+    NOTE: outFile cannot be condMerged_temp.parquet or condMerged_newCol.parquet
+    """
+
     if file_type == "parquet":
-        q1 = scan_parquet(f"{path_dat}{file_dat}")#, infer_schema_length=0)
+        q1 = scan_parquet(f"{path_dat}{file_dat}", low_memory=low_memory,)#, infer_schema_length=0)
     else:
-        q1 = scan_csv(f"{path_dat}{file_dat}", infer_schema_length=0)
-
-    for out_col, merge_cols in dict_merge.copy().items():
-        if len(merge_cols) > 1:
-            q1 = (
-                q1
-                .with_columns(
-                    col(merge_cols).str.strptime(plDate, "%Y-%m-%d"),
-                    plmin(merge_cols).alias(out_col)
-                )
-            )
-        else:
-            del dict_merge[out_col] #prevents deleting of unmerged cols
+        q1 = scan_csv(f"{path_dat}{file_dat}", infer_schema_length=0, low_memory=low_memory,)
 
     if low_memory:
-        q1 = (
-            q1
-            .select(col(["PATIENT_ID"] + list(dict_merge.keys())))
-            .collect().write_parquet(f"{path_dat}condMerged.parquet")
-        )
-        q1
+        for out_col, merge_cols in dict_merge.copy().items():
+            if logger is None:
+                print(out_col)
+            else:
+                logger.info(out_col)
 
-        del q1
+            if len(merge_cols) > 1:
+                (
+                    q1
+                    .select(
+                        col(["PRACTICE_PATIENT_ID"] + merge_cols)
+                        )
+                    .with_columns(
+                        col(merge_cols).str.strptime(plDate, "%Y-%m-%d"),
+                        plmin_horizontal(merge_cols).alias(out_col)
+                    )
+                    .select(
+                        col(["PRACTICE_PATIENT_ID", out_col,])
+                        )
+                    .collect()
+                    .write_parquet(f"{path_dat}condMerged_newCol.parquet")
+                )
+                (
+                    q1
+                    .select(
+                        col("*").exclude(merge_cols)
+                        )
+                    .join(
+                        scan_parquet(f"{path_dat}condMerged_newCol.parquet"),
+                        on = "PRACTICE_PATIENT_ID",
+                        how = "left",
+                        )
+                    .sink_parquet(f"{path_dat}condMerged_temp.parquet",)
 
-        file_1 = f"{path_dat}{file_dat}"
-        file_2 = f"{path_dat}condMerged.parquet"
-        joinCol = "PATIENT_ID"
+                )
+                del q1
+                rename(f"{path_dat}condMerged_temp.parquet",
+                       f"{path_dat}{outFile}",)
+                remove(f"{path_dat}condMerged_newCol.parquet")
 
-        toAdd = (
-            scan_parquet(file_2)
-        )
-        Combine = (
-            scan_parquet(file_1)
-            .join(toAdd, on=joinCol, how="left") #how="left" supports .sink_parquet and should be same as outer
-            .sink_parquet(f"{path_dat}mergedDat.parquet")
-        )
-        Combine
+                q1 = scan_parquet(f"{path_dat}condMerged.parquet")#, infer_schema_length=0)
+            else:
+                del dict_merge[out_col] #prevents deleting of unmerged cols
 
     else:
+        for out_col, merge_cols in dict_merge.copy().items():
+            if len(merge_cols) > 1:
+                q1 = (
+                    q1
+                    .with_columns(
+                        col(merge_cols).str.strptime(plDate, "%Y-%m-%d"),
+                        plmin_horizontal(merge_cols).alias(out_col)
+                    )
+                )
+            else:
+                del dict_merge[out_col] #prevents deleting of unmerged cols
+
         rm_cols = list(dict_merge.values())
         rm_cols = [x for sublist in rm_cols for x in sublist]
         q1 = (
             q1
             .select(col("*").exclude(rm_cols))
-            .collect().write_parquet(f"{path_dat}condMerged.parquet")
+            .collect().write_parquet(f"{path_dat}{outFile}")
         )
         q1
     return "finished merging"
+
+def combineLevels(
+        path_dat: str,
+        file_dat: str,
+        dict_merge: dict, #column: newName:(level1, level2, ...), ...
+        file_type = "parquet",
+        outFile="dat_updatedLevels.parquet",
+        ):
+    if file_type == "parquet":
+        q1 = scan_parquet(f"{path_dat}{file_dat}")#, infer_schema_length=0)
+        q1 = (q1
+              .cast(plUtf8))
+    else:
+        q1 = scan_csv(f"{path_dat}{file_dat}", infer_schema_length=0)
+
+    for col_lab, to_combo in dict_merge.items():
+        for newLabel, combo in to_combo.items():
+            q1 = (
+                    q1
+                    .with_columns(
+                        (plwhen(col(col_lab).is_in(combo))
+                        .then(newLabel)
+                        .otherwise(col(col_lab)))
+                        .alias(col_lab)
+                        )
+                    )
+    q1.collect().write_parquet(f"{path_dat}{outFile}")
+
+
 
 def par_to_csv(file_noExtension):
     dataset = ds.dataset(f"{file_noExtension}.parquet", format="parquet")
--- main/preprocessing.py
+++ main/preprocessing.py
@@ -3,12 +3,25 @@ import datetime
 import csv
 import multiprocessing as mp
 from itertools import repeat
-#import gc
+import logging
+from re import sub
 
 import polars as pl
-from DexterProcessing import process_ethImd
+from DexterProcessing import process_imd
+from DexterProcessing import rmDup
+from DexterProcessing import mergeCols
+from DexterProcessing import combineLevels
 import pyarrow.dataset as ds
 import yaml
+
+## Log
+logging.basicConfig(filename="log_sBatch_1Python.txt",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logger = logging.getLogger()
+##
 
 with open("wdir.yml",
           "r",
@@ -18,19 +31,129 @@ with open("wdir.yml",
 PATH = config["PATH"]
 DIR_DATA = f"{PATH}{config['dir_data']}"
 
-process_ethImd(
-        config["processing"]["filename"],
-        DIR_DATA,
-        row_group_size=None,
+## Format Null and ColNames ####################################################
+logger.info("Formatting null values")
+
+if config['processing']["filename"] is None:
+    filesToFormat = [f"{DIR_DATA}{config['processing']['filename_gold']}",
+                     f"{DIR_DATA}{config['processing']['filename_aurum']}"]
+    config['processing']['filename_gold'] = f"{config['processing']['filename_gold'][:-4]}_formNulls.parquet"
+    config['processing']['filename_aurum'] = f"{config['processing']['filename_aurum'][:-4]}_formNulls.parquet"
+else:
+    filesToFormat = [f"{DIR_DATA}{config['processing']['filename']}"]
+    config['processing']["filename"] = filesToFormat[0]
+
+for file_ in filesToFormat:
+    dat = (
+            pl.scan_csv(file_, infer_schema_length=0)
+            .with_columns(
+                pl.when(pl.all().str.len_chars() == 0)
+                    .then(None)
+                    .otherwise(pl.all())
+                    .name.keep()
+                )
+            )
+    # remove suffix from col names
+    dup_cols = ("BD_MEDI:CPRD_2RY_POLYCYTHAEMIA:266", "BD_MEDI:CPRD_B12_DEF:270",
+            "BD_MEDI:CPRDAURUM_ANGIODYSPLASIA_COLON:13",)
+    change_colnames = {k:"" for k in dat.columns if k.startswith("BD_MEDI:") and k not in dup_cols}
+    change_colnames = {k:sub(r":\d+$", "", k) for k in change_colnames.keys()}
+
+    dat = (
+            dat
+            # duplicate col to remove
+            .select(pl.all().exclude(dup_cols))
+            .rename(change_colnames)
+            )
+
+    dat.sink_parquet(f"{file_[:-4]}_formNulls.parquet")
+logger.info("    Formatting null values finished")
+del dat
+
+###LinkingAurumGold############################################################
+if config['processing']["filename"] is None:
+    print("Linking")
+    logger.info("Linking Gold and Aurum")
+
+    dat_a = config['processing']['filename_gold']
+    dat_b = config['processing']['filename_aurum']
+
+    outFile="dat_linked.parquet"
+    rmDup(
+        f"{DIR_DATA}{dat_a}",
+        f"{DIR_DATA}{dat_b}",
+        A_ind=0,
+        B_ind=2,
+        map_file=f"{DIR_DATA}{config['processing']['map_file_AtoB']}",
+        map_delim=config['processing']['map_delim_AtoB'],
         low_memory=False,
-        is_parquet=False,
-        toParquet=True,
-        file_map = "imd_mapping.csv",
+        wdir=DIR_DATA,
+        logger=logger,
+        outFile=outFile,
         )
+    logger.info("    Linking finished")
+else:
+    outFile = config["processing"]["filename"]
 
-#gc.collect()
+###MergeCols#####################################################################
+if outFile.find(".csv") != -1:
+    file_type = "csv"
+else:
+    file_type = "parquet"
 
-## Remove duplicate condition
-#dat = pl.scan_parquet(f"{DIR_DATA}dat_processed.parquet")
-#dat = dat.select(pl.all().exclude(["BD_MEDI:CPRD_2RY_POLYCYTHAEMIA:266", "B_MEDI:CPRD_2RY_POLYCYTHAEMIA:266", "B.M.CPRD_2RY_POLYCYTHAEMIA:266"]))
-#dat.collect(streaming=True).write_parquet(f"{DIR_DATA}dat_processed_clean.parquet")
+if config["processing"]["mergeCols_AtoB"] is not None:
+    print("Merging Cols")
+    logger.info("Merging columns")
+
+    mergeCols(
+            DIR_DATA,
+            outFile,
+            config["processing"]["mergeCols_AtoB"],
+            file_type = file_type,
+            low_memory = True, #False,
+            logger=logger,
+            outFile="condMerged.parquet",
+            )
+    outFile = "condMerged.parquet"
+    logger.info("    Merging Cols finished")
+
+###CombineLevels#####################################################################
+if outFile.find(".csv") != -1:
+    file_type = "csv"
+else:
+    file_type = "parquet"
+
+if config["processing"]["combineLevels"] is not None:
+    print("Processing Column Levels")
+    logger.info("Combining levels")
+
+    combineLevels(DIR_DATA,
+                  outFile,
+                  config["processing"]["combineLevels"],
+                  file_type=file_type,
+                  outFile="dat_updatedLevels.parquet",
+                  )
+    outFile = "dat_updatedLevels.parquet"
+    logger.info("    Processing Column Levels finished")
+
+###LinkImd#####################################################################
+if outFile.find(".csv") != -1:
+    is_parquet = False
+else:
+    is_parquet = True
+
+if config["processing"]['link_imd']:
+    print("Linking IMD")
+    logger.info("Linking IMD")
+
+    process_imd(
+            outFile,
+            DIR_DATA,
+            file_map = config["processing"]["imd_map_file"],
+            low_memory=False,
+            is_parquet=is_parquet,
+            logger=logger,
+            outFile="dat_processed.parquet",
+            )
+    outFile="dat_processed.parquet"
+    logger.info("    Linking IMD finished")
